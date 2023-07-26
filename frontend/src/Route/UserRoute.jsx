cd32a1c4139c7a56be0268901e3be4f31f56af87
meteor <zz5120937@gmail.com>
2023-07-26 17:26:15 +0800

    add UserRoute

---
+++ frontend/src/Route/UserRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AddMember from '../pages/user/AddMember'
+import Member from '../pages/user/Member'
+import UserPage from '../pages/user/UserPage'
+import SkillTree from '../pages/user/SkillTree'
+import Certificate from '../pages/user/Certificate'
+import Vendor from '../pages/user/Vendor'
+
+function UserRoutes({user, setUser, islogin, setIslogin}) {
+  return (
+    <Routes>
+      <Route path="/new" element={<AddMember />} />
+      <Route path="/list" element={<Member />}></Route>
+      <Route path={`/${user.firstName}`} element={<UserPage user={user} setUser={setUser} islogin={islogin} setIslogin={setIslogin} />}></Route>
+      <Route path="/skilltree" element={<SkillTree />}></Route>
+      <Route path="/certificates" element={<Certificate />}></Route>
+      <Route path="/vendors" element={<Vendor />}></Route>
+    </Routes>
+  )
+}
+
+export default UserRoutes
