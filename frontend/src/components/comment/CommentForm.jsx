730d4e4f0cf322264ab15ba577f3053cdfc3dcde
meteor <zz5120937@gmail.com>
2023-08-02 19:14:30 +0800

    update

--- frontend/src/components/comment/CommentForm.jsx
+++ frontend/src/components/comment/CommentForm.jsx
@@ -8,6 +8,8 @@ const CommentForm = ({ PostData }) => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
   const [newComment, setNewComment] = useState([]);
   const [commentText, setCommentText] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submissionMessage, setSubmissionMessage] = useState('');
 
   const requierdInformation = {
     method: 'post',
@@ -19,29 +21,38 @@ const CommentForm = ({ PostData }) => {
   };
 
   const updateComment = async () => {
+    setIsSubmitting(true); // Set isSubmitting to true before submitting, indicating that it is being submitted
     setNewComment({
       content: commentText,
       user: user,
       post: PostData
     });
-
+    console.log(commentText);
+    console.log(user);
+    console.log(PostData);
     try {
       const response = await fetch('/api/comment/', requierdInformation);
       if (!response.ok) throw new Error('Error updating comment');
       const comment = await response.json();
       setNewComment(comment);
+      setSubmissionMessage('successfully!'); // When susses set message
       console.log(comment);
     } catch (e) {
       console.log(e);
+      setSubmissionMessage('Failed'); // when false set message
+    } finally {
+      setIsSubmitting(false); // Only when isSubmitting is false, the submit operation is performed
     }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(newComment);
-    updateComment();
-    // 清空評論文字
-    setCommentText('');
+    if (!isSubmitting) { // Only when isSubmitting is false, the submit operation is performed
+      console.log(newComment);
+      updateComment();
+      // clear Textarea
+      setCommentText('');
+    }
   };
 
   return (
@@ -60,17 +71,25 @@ const CommentForm = ({ PostData }) => {
               onChange={(e) => setCommentText(e.target.value)}
             ></textarea>
           </div>
-          <div className="d-flex align-items-center ms-2 mt-4 pt-2 ">
-            <button type="submit" className="btn btn-primary">
-              Submit
+          <div className="d-flex align-items-center ms-2 mt-4 pt-2">
+            <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
+              {isSubmitting ? 'Submitting...' : 'Submit'}
             </button>
           </div>
+          {submissionMessage && (
+          <div className="text-center ms-2 mt-5 pt-1 d-flex align-items-center">
+            <p>{submissionMessage}</p>
+          </div>
+        )}
         </div>
+        
       </form>
     </Container>
   );
 };
 
-CommentForm.propTypes = {};
+CommentForm.propTypes = {
+  PostData: PropTypes.any.isRequired
+};
 
 export default CommentForm;
